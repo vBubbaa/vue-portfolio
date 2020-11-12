bc6832c2bd53f95d626483448ab6bf95f095d84a
vBubbaa <itylerjames@gmail.com>
2020-11-11 22:03:36 -0800

    Started with projects page + got image binding

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,27 @@
 <template>
   <v-container>
-    <h1>Projects</h1>
+    <div v-for="project in projects" :key="project.id">
+      <v-img
+        :src="require(`@/assets/projectimages/${project.image}`)"
+        width="150"
+        height="150"
+        contain
+      ></v-img>
+      {{ project.name }}
+    </div>
   </v-container>
 </template>
 
 <script>
+import projects from '../content/projects.json';
+
 export default {
   name: 'Projects',
+  data() {
+    return {
+      projects,
+    };
+  },
 };
 </script>
 
